b7800ab26b1e5176da22c9031e97eb58e6059582
Jack Cook <jack@jackcook.us>
2014-10-09 19:52:04 -0400

    Updated send.py
    This hasn't been tested yet

--- send.py
+++ send.py
@@ -1,25 +1,20 @@
+from aplo import *
+
 import re
 import smtplib
 import socket
 import sys
 from evernote.api.client import EvernoteClient
 
-fromemail = "INSERT_EMAIL_ADDRESS_HERE"
+fromemail = retrieveFromOptions('email_address')
 to = [sys.argv[1]]
 subject = "Error Log - %s" % socket.gethostname()
 
-dev_token = "INSERT_EVERNOTE_DEV_TOKEN_HERE"
-noteguid = ""
+dev_token = retrieveFromOptions('evernote_dev_token')
+noteguid = retrieveFromOptions('noteguid')
 
 client = EvernoteClient(token=dev_token)
 note_store = client.get_note_store()
-
-with open('data.txt', 'r+') as f:
-    for line in f.readlines():
-        key = line.split('=')[0]
-        value = line.split('=')[1].replace('\n', '')
-        if key == 'noteguid':
-            noteguid = value
 
 text = note_store.getNoteContent(dev_token, noteguid).replace('<br clear="none"/>', '\n')
 text = re.sub('<[^>]+>', '', text)
@@ -39,8 +34,8 @@ Subject: %s
 %s
 """ % (fromemail, ", ".join(to), subject, text)
 
-server = smtplib.SMTP("INSERT_SMTP_SERVER_HERE")
+server = smtplib.SMTP(retrieveFromOptions('email_smtp_server'))
 server.starttls()
-server.login('INSERT_EMAIL_ADDRESS_HERE', 'INSERT_EMAIL_PASSWORD_HERE')
+server.login(retrieveFromOptions('email_address'), retrieveFromOptions('email_password'))
 server.sendmail(fromemail, to, message)
 server.quit()
